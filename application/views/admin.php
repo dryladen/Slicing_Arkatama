09d5640c5c3330251280f33f1e5e8d97efa5e28c
dryladen <delfanladen24@gmail.com>
2022-11-24 23:01:08 +0800

    user access

--- application/views/admin.php
+++ application/views/admin.php
@@ -1,19 +1,18 @@
-<div class="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark" style="width: 280px; height: auto;">
-    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-      <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
-      <span class="fs-4">Webkuy</span>
+<!-- Sidebar -->
+<div class="row">
+  <div class="col-1 vh-100 d-flex flex-column flex-shrink-0 p-3 text-white bg-blue" style="width: 280px;">
+    <a href="/" class="d-flex align-items-center justify-content-sm-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+      <span class="fs-4 fw-bold">Webkuy</span>
     </a>
     <hr>
     <ul class="nav nav-pills flex-column mb-auto">
       <li>
         <a href="#" class="nav-link text-white">
-          <svg class="bi me-2" width="16" height="16"><use xlink:href="#speedometer2"/></svg>
           Dashboard
         </a>
       </li>
       <li>
         <a href="#" class="nav-link text-white">
-          <svg class="bi me-2" width="16" height="16"><use xlink:href="#people-circle"/></svg>
           Management Users
         </a>
       </li>
@@ -28,8 +27,16 @@
         <li><a class="dropdown-item" href="#">New project...</a></li>
         <li><a class="dropdown-item" href="#">Settings</a></li>
         <li><a class="dropdown-item" href="#">Profile</a></li>
-        <li><hr class="dropdown-divider"></li>
+        <li>
+          <hr class="dropdown-divider">
+        </li>
         <li><a class="dropdown-item" href="<?= base_url('auth/logout') ?>">Sign out</a></li>
       </ul>
     </div>
   </div>
+  <div class="col">
+    
+  </div>
+</div>
+
+<!-- Main Section -->
